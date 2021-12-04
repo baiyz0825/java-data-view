da573779a2d8f59551979ac251a0139433c7dc2a
BYZ0825 <byz0825@outlook.com>
2021-12-04 18:33:48 +0800

    添加Web层和Service业务层

---
+++ src/service/UserService.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * @author BaiYZ
+ * @program JavaDataView
+ * @description 处理业务
+ * @date 2021-12-04 18:31:57
+ */
+public interface UserService {
+}
---
+++ src/web/BaseServlet.java
@@ -0,0 +1,10 @@
+package web;
+
+/**
+ * @author BaiYZ
+ * @program JavaDataView
+ * @description 基础Servlet提供分发，使用反射获取对应的Service方法
+ * @date 2021-12-04 18:32:46
+ */
+public class BaseServlet {
+}
